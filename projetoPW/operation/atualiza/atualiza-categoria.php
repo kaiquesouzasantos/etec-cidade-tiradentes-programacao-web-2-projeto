98ef2c2e83491aa5d29d505d5276e724f5c66e36
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-11-09 15:18:17 -0300

    Delete projetoPW directory

--- projetoPW/operation/atualiza/atualiza-categoria.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("../../router.php");
-
-    if(CategoriaController::verificaPreenchimento()){
-        CategoriaDAO::atualizarCategoria($_POST['codigoCategoria'], $_POST['nomeCategoria']);
-    }
-
-    header("Location: ../../view/dashboard/pages/forms/cadastro-categoria.php");
-?>
--- projetoPW/operation/cadastro/cadastro-categoria.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("../../router.php");
-
-    if(CategoriaController::verificaPreenchimento()){
-        CategoriaDAO::cadastrarCategoria($_POST['nomeCategoria']);
-    }
-
-    header("Location: ../../view/dashboard/pages/forms/cadastro-categoria.php");
-?>
